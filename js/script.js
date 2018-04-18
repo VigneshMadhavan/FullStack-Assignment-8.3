a9fdff46101bf43cf032380bbf9891e692371585
VigneshMadhavan <vigneshmadhavan@yahoo.co.in>
2018-04-18 20:02:40 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function printTimesTable(n) {
+	//write a logic to print the n Multiplication Table
+	for (var i = 1; i <= 10; i++) {
+		console.log(n + ' * ' + i + ' = ' + (n * i))
+	}
+}
+///calll the printTimesTable function 
+printTimesTable(5);
